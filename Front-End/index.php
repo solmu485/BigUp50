75cf7b121651860fa02c8e2975baadc340084d99
Stefanetti Franjo <88632931+Nettilux@users.noreply.github.com>
2024-03-27 08:15:40 +0100

    Update index.php

--- Front-End/index.php
+++ Front-End/index.php
@@ -1,3 +1,42 @@
-<?php
- echo "hello";
-?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Stefanetti Franjo">
+    
+    <title>Brwal Talk</title>
+
+</head>
+<body>
+<main>
+
+        <?php
+        //includes um db credentials und Navigation fuer jede Seite 
+       // include_once "includes/db_credentials.php";
+       // include_once "includes/navigator.php";
+
+
+
+        $page = "";
+
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+        }
+
+
+        //if um Navigation Dynamisch zu erstellen
+        if ($page == "home") {
+            include_once "includes/home.php";
+        }  else {
+            include_once "includes/home.php";
+        }
+
+        //einfuegen des Footer fuer jede Seite
+        include_once "includes/footer.php";
+
+        ?>
+</main>
+</body>
+</html>
+
