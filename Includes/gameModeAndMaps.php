c6e0aebd3452c9f2b96d880da1f97e0314a63255
StalinEnjoyer <jwalers@outlook.de>
2024-05-07 22:57:06 +0200

    maps and gamemode
    added maps and gamemode to dev

---
+++ Includes/gameModeAndMaps.php
@@ -0,0 +1,23 @@
+<?php
+/** @author Walers Jean */
+echo '
+<head>
+
+    <title>Gamemodes and Maps</title>
+    <link rel="stylesheet" href="../styles/stylesheet_maps.css">
+</head>
+<body>
+<h1>ISS Location</h1>
+<div id="iss-location"></div>
+
+<h1>Gamemodes and their Maps</h1>
+
+<div id="game-modes"></div>
+
+<script src="../Front-End/issLocation.js"></script>
+<script src="../Front-End/gameModeAndMaps.js"></script>
+
+</body>
+'
+?>
+
---
+++ Includes/maps.php
@@ -0,0 +1,22 @@
+
+<?php
+/** @author Walers Jean */
+echo '
+<head>
+
+    <title>Maps</title>
+    <link rel="stylesheet" href="../styles/stylesheet_maps.css">
+
+</head>
+<body>
+<h1>Brawl Star Maps</h1>
+
+<div id="output" class="image-container"></div>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="../Front-End/maps.js"></script>
+
+</body>
+'
+?>
