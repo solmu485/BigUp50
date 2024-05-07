568753a39ebeaf84def6cd2c62a3f7d4bec0b53b
solmu485 <160712645+solmu485@users.noreply.github.com>
2024-05-07 21:59:01 +0200

    Add files via upload

--- Front-End/includes/Gachahome.php
+++ Front-End/includes/Gachahome.php
@@ -5,25 +5,8 @@
     <link rel="stylesheet" type="text/css" href="../Styles/Stylemusa.css" media="screen" />
     <?php
     include_once "navigation.php";
+    include_once "../../Back-End/includes/functiongacha.php";
     ?>
-    <script>
-
-        function pickRandomPrize() {
-            // Hide the image
-            document.getElementById("boxImage").style.display = "none";
-
-            // Send an AJAX request to the PHP script
-            var request = new XMLHttpRequest();
-            request.open("GET", "../../Back-End/includes/Gacha.php", true);
-            request.onreadystatechange = function() {
-                if (request.readyState == 4 && request.status == 200) {
-                    // Update the content of the result div with the response
-                    document.getElementById("result").innerHTML = request.responseText;
-                }
-            };
-            request.send();
-        }
-    </script>
 </head>
 <body>
 <h1>Random Prize Picker</h1>
