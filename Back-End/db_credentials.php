c925de67335829b059a47a64a92fa3da26fc641b
solmu485 <160712645+solmu485@users.noreply.github.com>
2024-05-07 21:32:13 +0200

    Delete Back-End/db_credentials.php

--- Back-End/db_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Verbindung zur Datenbank herstellen
-define('DB_HOST', 'localhost');
-define('DB_USER', 'stefr407');
-define('DB_PW', '2002122409031');
-define('DB_NAME', 'stefr407');
-
-?>
