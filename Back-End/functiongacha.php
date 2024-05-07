2ba218a3420ab36073d9c3d8f455d321a91cfd1e
solmu485 <160712645+solmu485@users.noreply.github.com>
2024-05-07 22:41:35 +0200

    Add files via upload

--- Back-End/functiongacha.php
+++ Back-End/functiongacha.php
@@ -6,7 +6,7 @@
 
         // Send an AJAX request to the PHP script
         var request = new XMLHttpRequest();
-        request.open("GET", "../../Back-End/includes/Gacha.php", true);
+        request.open("GET", "../Back-End/includes/Gacha.php", true);
         request.onreadystatechange = function() {
             if (request.readyState == 4 && request.status == 200) {
                 // Update the content of the result div with the response
