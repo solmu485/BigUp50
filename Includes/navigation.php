4897b3d38842495aab27e7c7d197493dde58f79f
BodyKicker <152383301+BodyKicker@users.noreply.github.com>
2024-05-07 22:40:08 +0200

    Update navigation.php

--- Includes/navigation.php
+++ Includes/navigation.php
@@ -11,7 +11,8 @@
 <div class="navbar">
     <a href="home.php?page=Gacha">Home</a>
     <a href="Gachahome.php?page=Gacha">Gacha</a>
-    <a href="">Brawler</a>
+    <a href="Char_Search.php">Character Suche/a>
+    <a href="ranking.php">Ranking</a>
     <a href="#">Placeholder</a>
     <a href="#">Placeholder</a>
 </div>
