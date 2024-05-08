06c16f375f7855f291886ebc3c16dc6430ce7472
solmu485 <160712645+solmu485@users.noreply.github.com>
2024-05-08 07:58:46 +0200

    Update navigation.php

--- Includes/navigation.php
+++ Includes/navigation.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" name="Musa und Franjo">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Website</title>
     <link rel="stylesheet" type="text/css" href="../Styles/Stylemusa.css">
