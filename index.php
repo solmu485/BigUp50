a248f25bec851aa03419bd7c6d90dc9f1f268be1
solmu485 <160712645+solmu485@users.noreply.github.com>
2024-05-10 12:22:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" name="Musa">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
